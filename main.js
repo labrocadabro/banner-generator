6b761720cd04bfc1eda303326791dbdcfdc0cf15
Laura <moomal.abro@gmail.com>
2022-10-01 15:00:23 +0500

    basic functionality complete

---
+++ main.js
@@ -0,0 +1,26 @@
+import { unsplashKey } from "./apiKey.js";
+
+const getPhotoButton = document.getElementById('get-photo');
+const addTextButton = document.getElementById('add-text');
+getPhotoButton.addEventListener('click', getPhoto);
+addTextButton.addEventListener('click', addText);
+
+
+async function getPhoto() {
+	const page = Math.ceil(Math.random() * 100);
+	const query = document.getElementById('photo-search').value.trim();
+	const res = await fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${unsplashKey}`);
+	const data = await res.json();
+	const img = data.results[Math.floor(Math.random() * 10)];
+	const bannerImg = document.getElementById('banner-img');
+	const bannerCredits = document.getElementById('banner-credits');
+	bannerImg.innerHTML = `<img src="${img.urls.raw}" alt="${img.alt_description}" />`;
+	bannerCredits.innerHTML = `Photo by <a href="${img.user.links.html}?utm_source=banner_generator&utm_medium=referral">${img.user.name}</a> on <a href="https://unsplash.com/?utm_source=banner_generator&utm_medium=referral">Unsplash</a> | <a href="${img.links.download}">Download Image</a>`;
+}
+
+function addText() {
+	const text = document.getElementById('text-value').value;
+	bannerText = document.getElementById('banner-text');
+	console.log(bannerText)
+	bannerText.innerHTML = text;
+}
